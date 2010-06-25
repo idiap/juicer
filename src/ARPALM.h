2aba328815ae5eecc8ab616eac4db67705f0ba0e
Phil Garner <Phil.Garner@idiap.ch>
2010-06-25 12:59:05 +0200

    Re-indent file to make it easier to debug.
    This shouldn't introduce any functional change.

--- src/ARPALM.h
+++ src/ARPALM.h
@@ -17,55 +17,53 @@
 	$Id: ARPALM.h,v 1.5 2005/08/26 01:16:34 moore Exp $
 */
 
-namespace Juicer {
-
-
-typedef struct
+namespace Juicer
 {
-	real        log_prob ;
-	real        log_bo ;
-	int         *words ;
-} ARPALMEntry ;
-        
-
-/**
- * Representation of an ARPA N-gram language model.
- */
-class ARPALM
-{
-public:
-	DecVocabulary *vocab ;
-
-	int            order ;
-	ARPALMEntry    **entries ;
-	int            *n_ngrams ;
-
-	char           *unk_wrd ;
-	int            unk_id ;
-
-	int            n_unk_words ;
-	int            *unk_words ;
-
-	ARPALM( DecVocabulary *vocab_ ) ;
-	ARPALM( const char *arpa_fname , DecVocabulary *vocab_ , const char *unk_wrd_ ) ;
-	virtual ~ARPALM() ;
-
-	void readARPA( const char *arpa_fname ) ;
-	void calcUnkWords() ;
-	void printWarning() ;
-	void allocateEntries() ;
-	void writeBinary( const char *fname ) ;
-	void readBinary( const char *fname ) ;
-	void outputText() ;
-
-    void Normalise();
-
-private:
-    real RecursiveNormalise(int iLevel, int* iContext, real iBackOff);
-    bool           *words_in_lm ;
-};
+    typedef struct
+    {
+        real        log_prob ;
+        real        log_bo ;
+        int         *words ;
+    } ARPALMEntry ;
 
 
+    /**
+     * Representation of an ARPA N-gram language model.
+     */
+    class ARPALM
+    {
+    public:
+        DecVocabulary *vocab ;
+
+        int            order ;
+        ARPALMEntry    **entries ;
+        int            *n_ngrams ;
+
+        char           *unk_wrd ;
+        int            unk_id ;
+
+        int            n_unk_words ;
+        int            *unk_words ;
+
+        ARPALM( DecVocabulary *vocab_ ) ;
+        ARPALM( const char *arpa_fname , DecVocabulary *vocab_ ,
+                const char *unk_wrd_ ) ;
+        virtual ~ARPALM() ;
+
+        void readARPA( const char *arpa_fname ) ;
+        void calcUnkWords() ;
+        void printWarning() ;
+        void allocateEntries() ;
+        void writeBinary( const char *fname ) ;
+        void readBinary( const char *fname ) ;
+        void outputText() ;
+
+        void Normalise();
+
+    private:
+        real RecursiveNormalise(int iLevel, int* iContext, real iBackOff);
+        bool           *words_in_lm ;
+    };
 }
 
 #endif
--- src/WFSTGramGen.h
+++ src/WFSTGramGen.h
@@ -20,102 +20,100 @@
 */
 
 
-namespace Juicer {
-
-
-typedef enum
+namespace Juicer
 {
-   WFST_GRAM_TYPE_WORDLOOP = 0 ,
-   WFST_GRAM_TYPE_SIL_WORDLOOP_SIL ,
-   WFST_GRAM_TYPE_NGRAM ,
-   WFST_GRAM_TYPE_WORDPAIR ,
-   WFST_GRAM_TYPE_INVALID
-} WFSTGramType ;
+    typedef enum
+    {
+        WFST_GRAM_TYPE_WORDLOOP = 0 ,
+        WFST_GRAM_TYPE_SIL_WORDLOOP_SIL ,
+        WFST_GRAM_TYPE_NGRAM ,
+        WFST_GRAM_TYPE_WORDPAIR ,
+        WFST_GRAM_TYPE_INVALID
+    } WFSTGramType ;
 
 
-struct WFSTNGramSMNode
-{
-   int   word ;   // index into vocab
-   int   state ;  // the state ID we have allocated to 
-   int   nextSib ;
-   int   firstChild ;
-   int   nOut ;
-   int   nIn ;
-};
+    struct WFSTNGramSMNode
+    {
+        int   word ;   // index into vocab
+        int   state ;  // the state ID we have allocated to
+        int   nextSib ;
+        int   firstChild ;
+        int   nOut ;
+        int   nIn ;
+    };
 
 
-class WFSTNGramStateManager
-{
-public:
-   WFSTNGramStateManager( DecVocabulary *vocab_ ) ;
-   virtual ~WFSTNGramStateManager() ;
+    class WFSTNGramStateManager
+    {
+    public:
+        WFSTNGramStateManager( DecVocabulary *vocab_ ) ;
+        virtual ~WFSTNGramStateManager() ;
 
-   int getWFSTState( int nWords , int *words , bool isFromState , 
-                     bool *isNew=NULL , bool isFinalLookup=false ) ;
-   int lookupWFSTState( int nWords , int *words ) ;
-   int getInitState() ;
-   int getNumStates() { return nStates ; } ;
-   int getEpsState() { return epsilonState ; } ;
-   void outputText() ;
-   void outputNonAccessible() ;
-   
-private:
-   DecVocabulary     *vocab ;
+        int getWFSTState( int nWords , int *words , bool isFromState ,
+                          bool *isNew=NULL , bool isFinalLookup=false ) ;
+        int lookupWFSTState( int nWords , int *words ) ;
+        int getInitState() ;
+        int getNumStates() { return nStates ; } ;
+        int getEpsState() { return epsilonState ; } ;
+        void outputText() ;
+        void outputNonAccessible() ;
 
-   int               *firstLevelNodes ;
+    private:
+        DecVocabulary     *vocab ;
 
-   int               nNodes ;
-   int               nNodesAlloc ;
-   WFSTNGramSMNode   *nodes ;
+        int               *firstLevelNodes ;
 
-   int               nStates ;
-   int               epsilonState ;
-   int               initState ;
+        int               nNodes ;
+        int               nNodesAlloc ;
+        WFSTNGramSMNode   *nodes ;
 
-   int getNode( int parentNode , int word , bool addNew=true ) ;
-   int allocNode( int word ) ;
-   void initNode( int ind ) ;
-   void outputNode( int node , int *nWords , int *words ) ;
-   void outputNonAccessibleNode( int node , int *nWords , int *words ) ;
-};
+        int               nStates ;
+        int               epsilonState ;
+        int               initState ;
+
+        int getNode( int parentNode , int word , bool addNew=true ) ;
+        int allocNode( int word ) ;
+        void initNode( int ind ) ;
+        void outputNode( int node , int *nWords , int *words ) ;
+        void outputNonAccessibleNode( int node , int *nWords , int *words ) ;
+    };
 
 
-class WFSTGramGen
-{
-public:
-   WFSTGramGen(
-       DecVocabulary *vocab_ , WFSTGramType type_ , real lmScale_ , 
-       real wordInsPen_ , const char *fName=NULL , const char *unkWord_=NULL
-   ) ;
-   virtual ~WFSTGramGen() ;
+    class WFSTGramGen
+    {
+    public:
+        WFSTGramGen(
+            DecVocabulary *vocab_ , WFSTGramType type_ , real lmScale_ ,
+            real wordInsPen_ , const char *fName=NULL , const char *unkWord_=NULL
+        ) ;
+        virtual ~WFSTGramGen() ;
 
-   void writeFSM(
-       const char *fsmFName , const char *inSymbolsFName , 
-       const char *outSymbolsFName ,
-       bool addSil=false , bool phiBOTrans=false, bool normalise=false
-   ) ;
-   int getPhiLabel() { return phiLabel ; } ;
+        void writeFSM(
+            const char *fsmFName , const char *inSymbolsFName ,
+            const char *outSymbolsFName ,
+            bool addSil=false , bool phiBOTrans=false, bool normalise=false
+        ) ;
+        int getPhiLabel() { return phiLabel ; } ;
 
-private:
-   WFSTGramType   type ;
-   DecVocabulary  *vocab ;
-   real           wordInsPen ;
-   real           lmScale ;
-   char           *unkWord ;
-   char           *arpaLMFName ;
-   char           *wordPairFName ;
-   int            phiLabel ;
+    private:
+        WFSTGramType   type ;
+        DecVocabulary  *vocab ;
+        real           wordInsPen ;
+        real           lmScale ;
+        char           *unkWord ;
+        char           *arpaLMFName ;
+        char           *wordPairFName ;
+        int            phiLabel ;
 
-   void writeFSMWordLoop( FILE *fsmFD ) ;
-   void writeFSMSilWordLoopSil( FILE *fsmFD ) ;
-   void writeFSMARPA(
-       FILE *fsmFD , bool addSil , bool phiBOTrans, bool normalise
-   ) ;
-   void addDefaultBackoffPath( FILE *fsmFD , WFSTNGramStateManager *stateMan , 
-                               int fromSt , int toNWords , int *toWords ) ; 
-   void writeFSMWordPair( FILE *fsmFD ) ;
-};
-
+        void writeFSMWordLoop( FILE *fsmFD ) ;
+        void writeFSMSilWordLoopSil( FILE *fsmFD ) ;
+        void writeFSMARPA(
+            FILE *fsmFD , bool addSil , bool phiBOTrans, bool normalise
+        ) ;
+        void addDefaultBackoffPath( FILE *fsmFD , WFSTNGramStateManager *stateMan ,
+                                    int fromSt , int toNWords , int *toWords ) ;
+        void writeFSMWordPair( FILE *fsmFD ) ;
+    };
 
 }
 
